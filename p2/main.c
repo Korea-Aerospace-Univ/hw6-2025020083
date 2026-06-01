a104fc98e9049a161b6208bf4a65b7ac688fd9a7
2025020083 <seoyoonlee77@naver.com>
2026-06-01 15:11:06 +0900

    2

--- p2/main.c
+++ p2/main.c
@@ -10,20 +10,22 @@ int main() {
     for (int *p = a; p < a + n; p++)
         scanf("%d", p);
 
-    for (int *p = b; p < b + n; p++)
+    for (p = b; p < b + n; p++)
         scanf("%d", p);
 
-    p=a;
-    q=b+n-1;
+    p = a;         
+    q = b + n - 1; 
 
-    while(p<a+n){
+    while (p < a + n) {
 
-      printf("%d", *p+*q);
+        printf("%d ", *p + *q);
 
-      p++;
-      q--;
+        p++;  
+        q--;   
+    }
 
-      }
+    return 0;
+}
 
     return 0;
 }
