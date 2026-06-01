cc29128b5012334de93716d8297a3074b3c0626d
2025020083 <seoyoonlee77@naver.com>
2026-06-01 15:25:48 +0900

    2

---
+++ p2/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>     
+
+int main()
+{
+    int N;             
+    int a[20]=nullptr
+    int b[20]=nullptr;   
+    int *p, *q;        
+
+    scanf("%d", &N);    
+                       
+
+    for (p = a; p < a + N; p++)  
+    {
+        scanf("%d", p);           
+                                  
+    }
+
+    for (p = b; p < b + N; p++)  
+    {
+        scanf("%d", p);          
+    }
+
+    q = b + N - 1;               
+                                  
+
+    for (p = a; p < a + N; p++)   
+    {
+        printf(" %d", *p + *q);   
+                                 
+                               
+
+        q--;                      
+                                
+    }
+
+    return 0;                    
