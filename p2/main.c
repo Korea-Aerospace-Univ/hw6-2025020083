8ceda37c9867f82127b50dc15e21fdfe0ca4a666
2025020083 <seoyoonlee77@naver.com>
2026-06-01 15:17:51 +0900

    2

--- p2/main.c
+++ p2/main.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 
-int main(void) {
+int main() {
 
-    int n;                   
-    int a[20], b[20];       
+    int n;
+    int a[20], b[20];
+
+    int *p;
+    int *q;
 
     scanf("%d", &n);
 
-    for (int *p = a; p < a + n; p++)
+    for (p = a; p < a + n; p++)
         scanf("%d", p);
 
     for (p = b; p < b + n; p++)
         scanf("%d", p);
 
-    p = a;         
-    q = b + n - 1; 
+    p = a;          // a의 첫 원소
+    q = b + n - 1; // b의 마지막 원소
 
     while (p < a + n) {
 
         printf("%d ", *p + *q);
 
-        p++;  
-        q--;   
+        p++;   // 다음 원소
+        q--;   // 이전 원소
     }
 
     return 0;
